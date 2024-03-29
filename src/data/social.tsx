9767e921ac8c7e76fca646a27c554d187297b529
ngvanlyy <106430258+ngvanlyy@users.noreply.github.com>
2024-03-30 00:11:02 +0700

    remove soical

--- src/data/social.tsx
+++ src/data/social.tsx
@@ -1,11 +1,6 @@
-import CouseraIcon from '@/public/social/coursera.png'
-import DuolingoIcon from '@/public/social/duolingo.svg'
+
 import GithubIcon from '@/public/social/github.svg'
-import GoodreadsIcon from '@/public/social/goodreads.svg'
 import LinkedInIcon from '@/public/social/linkedin.svg'
-import Math2ITIcon from '@/public/social/math2it.png'
-import SOIcon from '@/public/social/so.svg'
-import TwitterIcon from '@/public/social/twitter.png'
 import { BadgeSocialProps } from '@notion-x/src/components/BadgeSocial'
 
 const socials: BadgeSocialProps[] = [
