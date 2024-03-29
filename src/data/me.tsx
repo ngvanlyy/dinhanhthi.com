dc49730c32fcaa0fbf0fa25ecdf82aee47ca5bfa
ngvanlyy <106430258+ngvanlyy@users.noreply.github.com>
2024-03-29 23:45:53 +0700

    edit me

--- src/data/me.tsx
+++ src/data/me.tsx
@@ -1,42 +1,35 @@
 export const me = {
-  name: 'Anh-Thi Dinh',
-  email: 'me@dinhanhthi.com',
-  website: 'https://dinhanhthi.com',
+  name: 'Nguyen Van Ly',
+  email: 'ngvanlyy@gmail.com',
+  website: 'https://ngvanlyy.id.vn',
   quote: 'I fail my way to success.',
-  github: 'https://github.com/dinhanhthi',
-  linkedin: 'https://www.linkedin.com/in/dinhanhthi',
-  twitter: 'https://twitter.com/dinhanhthi',
-  goodreads: 'https://www.goodreads.com/user/show/19630622-thi-dinh',
-  coursera: 'https://www.coursera.org/user/47f27dde0ecab9a78e2c93632d78a556',
-  stackexchange: 'https://stackexchange.com/users/1344291/anh-thi-dinh?tab=accounts',
+  github: 'https://github.com/ngvanlyy',
+  linkedin: 'https://www.linkedin.com/in/nguyen-van-ly-7a4a64175/',
   // Used in the home page
   shortIntro:
-    'I am a guy with a curious mind. I would like to learn something new every day. I also have a PhD in Applied Mathematics and currently work as a <span class=" font-normal text-amber-200">Data Scientist</span> and <span class=" font-normal text-amber-200">Full Stack Developer</span>. On this page, you can find the notes I made when I discovered something new in the fields of <span class=" font-normal text-amber-200">Data Science</span> and <span class=" font-normal text-amber-200">Web Development</span>.',
+    'I am a girl with a curious mind. I would like to learn something new every day. I am currently work as a <span class=" font-normal text-amber-200">Data Analyst</span>. On this page, you can find the notes I made when I discovered something new in the fields of <span class=" font-normal text-amber-200">Data Science</span>',
   // Used in the about page
   longIntro:
-    'I am <span class="font-normal text-amber-200">Thi</span>, a Vietnamese <span class=" font-normal text-amber-200">Data Scientist</span> with a curious mind. As an ever-learning worm, I am interested in researching and sharing content about <span class=" font-normal text-amber-200">Applied Mathematics</span>, <span class=" font-normal text-amber-200">Web Dev</span> and <span class=" font-normal text-amber-200">Data Science</span>. I have a PhD in Applied Mathematics from U. Sorbonne Paris Nord and am an avid self-learner in Data Science.',
+    'I am <span class="font-normal text-amber-200">Van Ly</span>, a Vietnamese <span class=" font-normal text-amber-200">Data Analyst </span> with a curious mind. As an ever-learning worm, I am interested in researching and sharing content about <span class=" font-normal text-amber-200">Applied Mathematics</span>, and <span class=" font-normal text-amber-200">Data Science</span>. I am an avid self-learner in Data Science.',
   // Display a badge everywhere
   lookingForJob: false, // true if you are looking for a job. A badge will be displayed on the home page
   lookingForJobLabel: 'Looking for a job', // or 'Available for hire'
   googleAnalytics: 'G-K3YRSB918B',
   coordinate: {
     from: {
-      label: 'Ben Tre, Vietnam'
-    },
-    location: {
-      label: 'Île-de-France, France'
+      label: 'Ha Noi, Vietnam'
     },
     github: {
-      label: 'github.com/dinhanhthi',
-      href: 'https://github.com/dinhanhthi'
+      label: 'github.com/ngvanlyy',
+      href: 'https://github.com/ngvanlyy'
     },
     linkedin: {
-      label: 'linkedin.com/in/dinhanhthi',
-      href: 'https://www.linkedin.com/in/dinhanhthi'
+      label: 'linkedin.com/in/ngvanlyy',
+      href: 'https://www.linkedin.com/in/nguyen-van-ly-7a4a64175/'
     },
     email: {
-      label: 'me@dinhanhthi.com',
-      href: 'mailto:me@dinhanhthi.com'
+      label: 'ngvanlyy@gmail.com',
+      href: 'mailto:ngvanlyy@gmail.com'
     }
   }
 }
